18de785a9ff398246493177e22e824760ca63228
jeFFFairson <jft@tisseur-de-toile.fr>
2018-11-06 16:27:19 +0100

    Add search engin on home view

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,24 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page class="">
+    <div class="row justify-center">
+      <div class="col-11">
+        <h4>TTmTo</h4>
+        <div class="row">
+          <div class="col-8 align-end">
+            <q-field>
+              <q-input v-model="search" float-label="Rechercher une adresse"/>
+            </q-field>
+          </div>
+          <div class="col-4 align-end">
+            <q-btn
+              label="->"
+              type="submit"
+              color="primary"
+              />
+          </div>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -9,6 +27,15 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      search: '',
+      isUserConnected: false
+    }
+  },
+  created () {
+    this.isUserConnected = this.$store.getters['userdata/isUserConnected']
+  }
 }
 </script>
