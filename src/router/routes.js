a10777e467897c111f26653ef43064ea0f4763b0
jeFFFairson <jft@tisseur-de-toile.fr>
2018-11-16 16:02:38 +0100

    Add register vue route

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,7 +8,8 @@ const routes = [
       { path: 'hotels', component: () => import('pages/Hotels.vue') },
       { path: 'hotels/:id', name: 'hotel', component: () => import('pages/Hotel.vue') },
       { path: 'test', component: () => import('pages/Test.vue') },
-      { path: 'connect', component: () => import('pages/Connect.vue') }
+      { path: 'connect', component: () => import('pages/Connect.vue') },
+      { path: 'register', component: () => import('pages/Register.vue') }
     ]
   }
 ]
